918614c080603d335d5122d0e381be1a18202738
ThienTu2512 <2151010425Tu@ou.edu.vn>
2023-10-25 07:46:03 +0700

    Tu dien Anh-Viet

---
+++ DictionaryApp/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace DictionaryApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btXoa = new System.Windows.Forms.Button();
+            this.btLuu = new System.Windows.Forms.Button();
+            this.btSua = new System.Windows.Forms.Button();
+            this.btTimKiem = new System.Windows.Forms.Button();
+            this.txtShortVN = new System.Windows.Forms.TextBox();
+            this.txtEnglish = new System.Windows.Forms.TextBox();
+            this.txtVN = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lsvDanhSach = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBox1.BackgroundImage")));
+            this.groupBox1.Controls.Add(this.txtOutput);
+            this.groupBox1.Controls.Add(this.txtInput);
+            this.groupBox1.Controls.Add(this.btXoa);
+            this.groupBox1.Controls.Add(this.btLuu);
+            this.groupBox1.Controls.Add(this.btSua);
+            this.groupBox1.Controls.Add(this.btTimKiem);
+            this.groupBox1.Controls.Add(this.txtShortVN);
+            this.groupBox1.Controls.Add(this.txtEnglish);
+            this.groupBox1.Controls.Add(this.txtVN);
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(1143, 327);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thêm Từ Mới";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(687, 142);
+            this.txtOutput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.PlaceholderText = "Nghĩa tiếng Việt";
+            this.txtOutput.Size = new System.Drawing.Size(255, 31);
+            this.txtOutput.TabIndex = 8;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(687, 70);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.PlaceholderText = "Nhập từ tiếng Anh";
+            this.txtInput.Size = new System.Drawing.Size(255, 31);
+            this.txtInput.TabIndex = 8;
+            // 
+            // btXoa
+            // 
+            this.btXoa.Location = new System.Drawing.Point(989, 213);
+            this.btXoa.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btXoa.Name = "btXoa";
+            this.btXoa.Size = new System.Drawing.Size(137, 82);
+            this.btXoa.TabIndex = 6;
+            this.btXoa.Text = "Xoá";
+            this.btXoa.UseVisualStyleBackColor = true;
+            this.btXoa.Click += new System.EventHandler(this.btXoa_Click);
+            // 
+            // btLuu
+            // 
+            this.btLuu.Location = new System.Drawing.Point(669, 213);
+            this.btLuu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btLuu.Name = "btLuu";
+            this.btLuu.Size = new System.Drawing.Size(137, 82);
+            this.btLuu.TabIndex = 7;
+            this.btLuu.Text = "Lưu";
+            this.btLuu.UseVisualStyleBackColor = true;
+            this.btLuu.Click += new System.EventHandler(this.btLuu_Click);
+            // 
+            // btSua
+            // 
+            this.btSua.Location = new System.Drawing.Point(830, 213);
+            this.btSua.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btSua.Name = "btSua";
+            this.btSua.Size = new System.Drawing.Size(137, 82);
+            this.btSua.TabIndex = 5;
+            this.btSua.Text = "Sửa";
+            this.btSua.UseVisualStyleBackColor = true;
+            this.btSua.Click += new System.EventHandler(this.btSua_Click);
+            // 
+            // btTimKiem
+            // 
+            this.btTimKiem.Location = new System.Drawing.Point(989, 57);
+            this.btTimKiem.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btTimKiem.Name = "btTimKiem";
+            this.btTimKiem.Size = new System.Drawing.Size(119, 62);
+            this.btTimKiem.TabIndex = 4;
+            this.btTimKiem.Text = "Tìm Kiếm";
+            this.btTimKiem.UseVisualStyleBackColor = true;
+            this.btTimKiem.Click += new System.EventHandler(this.btTimKiem_Click);
+            // 
+            // txtShortVN
+            // 
+            this.txtShortVN.Location = new System.Drawing.Point(209, 217);
+            this.txtShortVN.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtShortVN.Name = "txtShortVN";
+            this.txtShortVN.PlaceholderText = "Nhập nghĩa tiếng Việt";
+            this.txtShortVN.Size = new System.Drawing.Size(177, 31);
+            this.txtShortVN.TabIndex = 2;
+            // 
+            // txtEnglish
+            // 
+            this.txtEnglish.Location = new System.Drawing.Point(209, 142);
+            this.txtEnglish.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtEnglish.Name = "txtEnglish";
+            this.txtEnglish.PlaceholderText = "Nhập tiếng Anh";
+            this.txtEnglish.Size = new System.Drawing.Size(177, 31);
+            this.txtEnglish.TabIndex = 1;
+            // 
+            // txtVN
+            // 
+            this.txtVN.Location = new System.Drawing.Point(414, 97);
+            this.txtVN.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtVN.Multiline = true;
+            this.txtVN.Name = "txtVN";
+            this.txtVN.PlaceholderText = "Nhập nghĩa chi tiết";
+            this.txtVN.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txtVN.Size = new System.Drawing.Size(228, 196);
+            this.txtVN.TabIndex = 3;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(209, 70);
+            this.txtID.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtID.Name = "txtID";
+            this.txtID.PlaceholderText = "Nhập ID";
+            this.txtID.Size = new System.Drawing.Size(177, 31);
+            this.txtID.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(37, 220);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 32);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Dịch nghĩa:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(37, 145);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 32);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "English:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(413, 57);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(240, 32);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Dịch Nghĩa Chi Tiết:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(37, 73);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lsvDanhSach);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Location = new System.Drawing.Point(0, 337);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(1143, 413);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Danh Sách Từ Đã Có";
+            // 
+            // lsvDanhSach
+            // 
+            this.lsvDanhSach.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lsvDanhSach.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lsvDanhSach.FullRowSelect = true;
+            this.lsvDanhSach.GridLines = true;
+            this.lsvDanhSach.Location = new System.Drawing.Point(4, 29);
+            this.lsvDanhSach.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lsvDanhSach.Name = "lsvDanhSach";
+            this.lsvDanhSach.Size = new System.Drawing.Size(1135, 379);
+            this.lsvDanhSach.TabIndex = 0;
+            this.lsvDanhSach.UseCompatibleStateImageBehavior = false;
+            this.lsvDanhSach.View = System.Windows.Forms.View.Details;
+            this.lsvDanhSach.SelectedIndexChanged += new System.EventHandler(this.lsvDanhSach_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "English";
+            this.columnHeader2.Width = 200;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "ShortVN";
+            this.columnHeader3.Width = 200;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "VN";
+            this.columnHeader4.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1143, 750);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tra Từ Điển Anh-Việt";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Label label1;
+        private TextBox txtShortVN;
+        private TextBox txtEnglish;
+        private TextBox txtID;
+        private Label label3;
+        private Label label2;
+        private Button btXoa;
+        private Button btLuu;
+        private Button btSua;
+        private Button btTimKiem;
+        private TextBox txtVN;
+        private Label label4;
+        private ListView lsvDanhSach;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private TextBox txtOutput;
+        private TextBox txtInput;
+    }
+}
